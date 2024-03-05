16521e8fbe526b5af48453dc04bd27e6c4a28a34
skanhai <104988963+skanhai@users.noreply.github.com>
2024-03-04 22:32:21 -0500

    Update conversions.py
    pycharm push error taken directly from pycharm

--- conversions.py
+++ conversions.py
@@ -1,13 +1,17 @@
-
 def convertCelsiusToKelvin(celsius):
-    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Kelvins"""
-    kelvins = 0
-    
-    return kelvins
-
+    return celsius + 273.15
 
 def convertCelsiusToFahrenheit(celsius):
-    """Takes in a float representing a Celsius measurement, and returns that temperature converted into Fahrenheit"""
-    fahrenheit = 0
-    
-    return fahrenheit
+    return celsius * 9/5 + 32
+
+def convertFahrenheitToCelsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+def convertFahrenheitToKelvin(fahrenheit):
+    return (fahrenheit + 459.67) * 5/9
+
+def convertKelvinToCelsius(kelvin):
+    return kelvin - 273.15
+
+def convertKelvinToFahrenheit(kelvin):
+    return kelvin * 9/5 - 459.67
